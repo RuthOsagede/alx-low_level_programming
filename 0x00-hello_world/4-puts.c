84865207c00da552a94ee16ca3171ad6139a43e3
RuthOsagede <ruthosagede1@gmail.com>
2022-10-15 08:12:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * print a line of code using puts
+ *main - print a line of code using puts
  *
  * return: 0
  */
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-};
+}
