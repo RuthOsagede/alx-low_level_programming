d09e65caaf6b507ab94dae81a9d4c96597724d6d
RuthOsagede <ruthosagede1@gmail.com>
2022-10-16 13:37:41 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/*
+/**
  * main - print a line of code using puts
+ * Description: using a main function
  *
  * return: 0
  */
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a c program using printf
+ * description: using main funtion
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the output is a piece of art,\n");
+	return (0);
+}
